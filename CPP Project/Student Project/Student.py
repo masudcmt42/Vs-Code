cb5db5c3b2dc361a0eb5797cf51ca6f3f3968b8d
Masud Rana <masudcmt42@gmail.com>
2024-12-14 16:56:07 +0600

    Object fix

--- CPP Project/Student Project/Student.py
+++ CPP Project/Student Project/Student.py
@@ -1,12 +1,10 @@
+
 class Student:
     def __init__(self, Name,Id):
         self.name=Name
-<<<<<<< HEAD
         self.id-Id
         
-    print("Student name is:")
-=======
-        self.id-id
-print("Student name is:)
-print(Student.name)
->>>>>>> 1295e46596a6e01e88de38a8722fa7aa89c66626
+st= Student("Ankila",200012)
+
+print("Student name is:")
+print(st.name)
